3ed4b71ec2816cf099512b687b3a0dcaeb983a80
Romit_Dasari <51027370+romitdsari18@users.noreply.github.com>
2019-06-04 21:59:48 +0530

    Add files via upload

---
+++ LeapMotion-master/LeapMotionCoOrdinates/Leap/LeapMotionCoOrdinates/LeapMotionCoOrdinatesForm.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace LeapMotionCoOrdinates
+{
+    partial class LeapMotionCoOrdinatesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmdConnectLeapMotion = new System.Windows.Forms.Button();
+            this.handPosX = new System.Windows.Forms.TextBox();
+            this.handPosY = new System.Windows.Forms.TextBox();
+            this.handPosZ = new System.Windows.Forms.TextBox();
+            this.cmdDisconnectLeapMotion = new System.Windows.Forms.Button();
+            this.OriginZ = new System.Windows.Forms.TextBox();
+            this.OriginY = new System.Windows.Forms.TextBox();
+            this.OriginX = new System.Windows.Forms.TextBox();
+            this.PAxisZ = new System.Windows.Forms.TextBox();
+            this.PAxisY = new System.Windows.Forms.TextBox();
+            this.PAxisX = new System.Windows.Forms.TextBox();
+            this.QAxisZ = new System.Windows.Forms.TextBox();
+            this.QAxisY = new System.Windows.Forms.TextBox();
+            this.QAxisX = new System.Windows.Forms.TextBox();
+            this.cmdSetOrigin = new System.Windows.Forms.Button();
+            this.cmdSetAxis1 = new System.Windows.Forms.Button();
+            this.cmdSetAxis2 = new System.Windows.Forms.Button();
+            this.ScreenPosX = new System.Windows.Forms.TextBox();
+            this.ScreenPosY = new System.Windows.Forms.TextBox();
+            this.ScreenPosZ = new System.Windows.Forms.TextBox();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.distance = new System.Windows.Forms.TextBox();
+            this.cmdSetPoint1 = new System.Windows.Forms.Button();
+            this.cmdSetPoint2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdConnectLeapMotion
+            // 
+            this.cmdConnectLeapMotion.Location = new System.Drawing.Point(13, 13);
+            this.cmdConnectLeapMotion.Name = "cmdConnectLeapMotion";
+            this.cmdConnectLeapMotion.Size = new System.Drawing.Size(75, 23);
+            this.cmdConnectLeapMotion.TabIndex = 0;
+            this.cmdConnectLeapMotion.Text = "Connect";
+            this.cmdConnectLeapMotion.UseVisualStyleBackColor = true;
+            this.cmdConnectLeapMotion.Click += new System.EventHandler(this.cmdConnectLeapMotion_Click);
+            // 
+            // handPosX
+            // 
+            this.handPosX.Location = new System.Drawing.Point(13, 43);
+            this.handPosX.Name = "handPosX";
+            this.handPosX.Size = new System.Drawing.Size(100, 22);
+            this.handPosX.TabIndex = 1;
+            // 
+            // handPosY
+            // 
+            this.handPosY.Location = new System.Drawing.Point(13, 72);
+            this.handPosY.Name = "handPosY";
+            this.handPosY.Size = new System.Drawing.Size(100, 22);
+            this.handPosY.TabIndex = 2;
+            // 
+            // handPosZ
+            // 
+            this.handPosZ.Location = new System.Drawing.Point(13, 101);
+            this.handPosZ.Name = "handPosZ";
+            this.handPosZ.Size = new System.Drawing.Size(100, 22);
+            this.handPosZ.TabIndex = 3;
+            // 
+            // cmdDisconnectLeapMotion
+            // 
+            this.cmdDisconnectLeapMotion.Location = new System.Drawing.Point(143, 12);
+            this.cmdDisconnectLeapMotion.Name = "cmdDisconnectLeapMotion";
+            this.cmdDisconnectLeapMotion.Size = new System.Drawing.Size(97, 23);
+            this.cmdDisconnectLeapMotion.TabIndex = 4;
+            this.cmdDisconnectLeapMotion.Text = "Disconnect";
+            this.cmdDisconnectLeapMotion.UseVisualStyleBackColor = true;
+            this.cmdDisconnectLeapMotion.Click += new System.EventHandler(this.cmdDisconnectLeapMotion_Click);
+            // 
+            // OriginZ
+            // 
+            this.OriginZ.Location = new System.Drawing.Point(13, 214);
+            this.OriginZ.Name = "OriginZ";
+            this.OriginZ.Size = new System.Drawing.Size(100, 22);
+            this.OriginZ.TabIndex = 7;
+            // 
+            // OriginY
+            // 
+            this.OriginY.Location = new System.Drawing.Point(13, 185);
+            this.OriginY.Name = "OriginY";
+            this.OriginY.Size = new System.Drawing.Size(100, 22);
+            this.OriginY.TabIndex = 6;
+            // 
+            // OriginX
+            // 
+            this.OriginX.Location = new System.Drawing.Point(13, 156);
+            this.OriginX.Name = "OriginX";
+            this.OriginX.Size = new System.Drawing.Size(100, 22);
+            this.OriginX.TabIndex = 5;
+            // 
+            // PAxisZ
+            // 
+            this.PAxisZ.Location = new System.Drawing.Point(143, 214);
+            this.PAxisZ.Name = "PAxisZ";
+            this.PAxisZ.Size = new System.Drawing.Size(100, 22);
+            this.PAxisZ.TabIndex = 10;
+            // 
+            // PAxisY
+            // 
+            this.PAxisY.Location = new System.Drawing.Point(143, 185);
+            this.PAxisY.Name = "PAxisY";
+            this.PAxisY.Size = new System.Drawing.Size(100, 22);
+            this.PAxisY.TabIndex = 9;
+            // 
+            // PAxisX
+            // 
+            this.PAxisX.AcceptsReturn = true;
+            this.PAxisX.Location = new System.Drawing.Point(143, 156);
+            this.PAxisX.Name = "PAxisX";
+            this.PAxisX.Size = new System.Drawing.Size(100, 22);
+            this.PAxisX.TabIndex = 8;
+            // 
+            // QAxisZ
+            // 
+            this.QAxisZ.Location = new System.Drawing.Point(294, 214);
+            this.QAxisZ.Name = "QAxisZ";
+            this.QAxisZ.Size = new System.Drawing.Size(100, 22);
+            this.QAxisZ.TabIndex = 13;
+            // 
+            // QAxisY
+            // 
+            this.QAxisY.Location = new System.Drawing.Point(294, 185);
+            this.QAxisY.Name = "QAxisY";
+            this.QAxisY.Size = new System.Drawing.Size(100, 22);
+            this.QAxisY.TabIndex = 12;
+            // 
+            // QAxisX
+            // 
+            this.QAxisX.Location = new System.Drawing.Point(294, 156);
+            this.QAxisX.Name = "QAxisX";
+            this.QAxisX.Size = new System.Drawing.Size(100, 22);
+            this.QAxisX.TabIndex = 11;
+            // 
+            // cmdSetOrigin
+            // 
+            this.cmdSetOrigin.Location = new System.Drawing.Point(12, 243);
+            this.cmdSetOrigin.Name = "cmdSetOrigin";
+            this.cmdSetOrigin.Size = new System.Drawing.Size(101, 23);
+            this.cmdSetOrigin.TabIndex = 14;
+            this.cmdSetOrigin.Text = "Set Origin";
+            this.cmdSetOrigin.UseVisualStyleBackColor = true;
+            this.cmdSetOrigin.Click += new System.EventHandler(this.cmdSetOrigin_Click);
+            // 
+            // cmdSetAxis1
+            // 
+            this.cmdSetAxis1.Location = new System.Drawing.Point(143, 243);
+            this.cmdSetAxis1.Name = "cmdSetAxis1";
+            this.cmdSetAxis1.Size = new System.Drawing.Size(100, 23);
+            this.cmdSetAxis1.TabIndex = 15;
+            this.cmdSetAxis1.Text = "Set Axis 1";
+            this.cmdSetAxis1.UseVisualStyleBackColor = true;
+            this.cmdSetAxis1.Click += new System.EventHandler(this.cmdSetAxis1_Click);
+            // 
+            // cmdSetAxis2
+            // 
+            this.cmdSetAxis2.Location = new System.Drawing.Point(294, 243);
+            this.cmdSetAxis2.Name = "cmdSetAxis2";
+            this.cmdSetAxis2.Size = new System.Drawing.Size(100, 23);
+            this.cmdSetAxis2.TabIndex = 16;
+            this.cmdSetAxis2.Text = "Set Axis 2";
+            this.cmdSetAxis2.UseVisualStyleBackColor = true;
+            this.cmdSetAxis2.Click += new System.EventHandler(this.cmdSetAxis2_Click);
+            // 
+            // ScreenPosX
+            // 
+            this.ScreenPosX.Location = new System.Drawing.Point(294, 43);
+            this.ScreenPosX.Name = "ScreenPosX";
+            this.ScreenPosX.Size = new System.Drawing.Size(100, 22);
+            this.ScreenPosX.TabIndex = 17;
+            // 
+            // ScreenPosY
+            // 
+            this.ScreenPosY.Location = new System.Drawing.Point(294, 72);
+            this.ScreenPosY.Name = "ScreenPosY";
+            this.ScreenPosY.Size = new System.Drawing.Size(100, 22);
+            this.ScreenPosY.TabIndex = 18;
+            // 
+            // ScreenPosZ
+            // 
+            this.ScreenPosZ.Location = new System.Drawing.Point(294, 100);
+            this.ScreenPosZ.Name = "ScreenPosZ";
+            this.ScreenPosZ.Size = new System.Drawing.Size(100, 22);
+            this.ScreenPosZ.TabIndex = 19;
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(401, 12);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(386, 354);
+            this.zedGraphControl1.TabIndex = 20;
+            // 
+            // distance
+            // 
+            this.distance.Location = new System.Drawing.Point(267, 343);
+            this.distance.Name = "distance";
+            this.distance.Size = new System.Drawing.Size(100, 22);
+            this.distance.TabIndex = 22;
+            // 
+            // cmdSetPoint1
+            // 
+            this.cmdSetPoint1.Location = new System.Drawing.Point(38, 342);
+            this.cmdSetPoint1.Name = "cmdSetPoint1";
+            this.cmdSetPoint1.Size = new System.Drawing.Size(75, 23);
+            this.cmdSetPoint1.TabIndex = 24;
+            this.cmdSetPoint1.Text = "Point1";
+            this.cmdSetPoint1.UseVisualStyleBackColor = true;
+            this.cmdSetPoint1.Click += new System.EventHandler(this.CmdSetPoint1_Click);
+            // 
+            // cmdSetPoint2
+            // 
+            this.cmdSetPoint2.Location = new System.Drawing.Point(165, 343);
+            this.cmdSetPoint2.Name = "cmdSetPoint2";
+            this.cmdSetPoint2.Size = new System.Drawing.Size(75, 23);
+            this.cmdSetPoint2.TabIndex = 25;
+            this.cmdSetPoint2.Text = "Point2";
+            this.cmdSetPoint2.UseVisualStyleBackColor = true;
+            this.cmdSetPoint2.Click += new System.EventHandler(this.CmdSetPoint2_Click);
+            // 
+            // LeapMotionCoOrdinatesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmdSetPoint2);
+            this.Controls.Add(this.cmdSetPoint1);
+            this.Controls.Add(this.distance);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.ScreenPosZ);
+            this.Controls.Add(this.ScreenPosY);
+            this.Controls.Add(this.ScreenPosX);
+            this.Controls.Add(this.cmdSetAxis2);
+            this.Controls.Add(this.cmdSetAxis1);
+            this.Controls.Add(this.cmdSetOrigin);
+            this.Controls.Add(this.QAxisZ);
+            this.Controls.Add(this.QAxisY);
+            this.Controls.Add(this.QAxisX);
+            this.Controls.Add(this.PAxisZ);
+            this.Controls.Add(this.PAxisY);
+            this.Controls.Add(this.PAxisX);
+            this.Controls.Add(this.OriginZ);
+            this.Controls.Add(this.OriginY);
+            this.Controls.Add(this.OriginX);
+            this.Controls.Add(this.cmdDisconnectLeapMotion);
+            this.Controls.Add(this.handPosZ);
+            this.Controls.Add(this.handPosY);
+            this.Controls.Add(this.handPosX);
+            this.Controls.Add(this.cmdConnectLeapMotion);
+            this.Name = "LeapMotionCoOrdinatesForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdConnectLeapMotion;
+        private System.Windows.Forms.TextBox handPosX;
+        private System.Windows.Forms.TextBox handPosY;
+        private System.Windows.Forms.TextBox handPosZ;
+        private System.Windows.Forms.Button cmdDisconnectLeapMotion;
+        private System.Windows.Forms.TextBox OriginZ;
+        private System.Windows.Forms.TextBox OriginY;
+        private System.Windows.Forms.TextBox OriginX;
+        private System.Windows.Forms.TextBox PAxisZ;
+        private System.Windows.Forms.TextBox PAxisY;
+        private System.Windows.Forms.TextBox PAxisX;
+        private System.Windows.Forms.TextBox QAxisZ;
+        private System.Windows.Forms.TextBox QAxisY;
+        private System.Windows.Forms.TextBox QAxisX;
+        private System.Windows.Forms.Button cmdSetOrigin;
+        private System.Windows.Forms.Button cmdSetAxis1;
+        private System.Windows.Forms.Button cmdSetAxis2;
+        private System.Windows.Forms.TextBox ScreenPosX;
+        private System.Windows.Forms.TextBox ScreenPosY;
+        private System.Windows.Forms.TextBox ScreenPosZ;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.TextBox distance;
+        private System.Windows.Forms.Button cmdSetPoint1;
+        private System.Windows.Forms.Button cmdSetPoint2;
+    }
+}
+
---
+++ LeapMotion-master/LeapMotionCoOrdinates/Leap/LeapMotionCoOrdinates/LeapMotionCoOrdinatesForm.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Leap;
+using System.Drawing.Imaging;
+using ZedGraph;
+
+namespace LeapMotionCoOrdinates
+{
+    public partial class LeapMotionCoOrdinatesForm : Form
+    {
+        private Controller controller;
+        GraphPane graphPane;
+        Hand hand;
+        Vector Leap = new Vector();
+        Vector Screen = new Vector();
+        Vector _OriginPoint = new Vector();
+        Vector AxisX;
+        Vector AxisZ;
+        float[,] _orientationScreen = new float[3, 3];
+        float[,] _orientationScreenTransposed = new float[3, 3];
+        float[,] _transformationInverse = new float[4, 4];
+        
+        bool isTransformed = false;
+
+        Vector Point1 = new Vector();
+        Vector Point2 = new Vector();
+
+        public LeapMotionCoOrdinatesForm()
+        {
+            InitializeComponent();
+            graphPane = zedGraphControl1.GraphPane;
+
+            DrawcartesianLines();
+        }
+        private void DrawcartesianLines()
+        {
+            graphPane.CurveList.Clear();
+
+            zedGraphControl1.GraphPane.YAxis.MajorGrid.IsZeroLine = true;
+            zedGraphControl1.GraphPane.XAxis.MajorGrid.IsZeroLine = true;
+            zedGraphControl1.GraphPane.XAxis.Scale.Max = 500;
+            zedGraphControl1.GraphPane.XAxis.Scale.Min = -500;
+            zedGraphControl1.GraphPane.YAxis.Scale.Max = 500;
+            zedGraphControl1.GraphPane.YAxis.Scale.Min = -500;
+        }
+        private void cmdConnectLeapMotion_Click(object sender, EventArgs e)
+        {
+
+             controller = new Controller();
+
+            controller.Connect += Controller_Connect;
+            controller.Device += Controller_Device;
+            controller.FrameReady += Controller_FrameReady;
+        }
+
+        private void Controller_FrameReady(object sender, FrameEventArgs e)
+        {
+            Frame frame = e.frame;
+
+            if (frame.Hands.Count > 0)
+            {
+                hand = frame.Hands[0];
+                if (hand.Fingers.Count > 0)
+                {
+                    Leap.x = Convert.ToSingle(Math.Round(hand.PalmPosition.x, 3));
+                    Leap.y = Convert.ToSingle(Math.Round(hand.PalmPosition.z, 3));
+                    Leap.z = Convert.ToSingle(Math.Round(hand.PalmPosition.y, 3));
+                    handPosX.Text = Leap.x.ToString();
+                    handPosY.Text = Leap.y.ToString();
+                    handPosZ.Text = Leap.z.ToString();
+                }
+                if (isTransformed)
+                {
+                    //testing
+                    //Leap.x = 0;
+                    //Leap.y = 0;
+                    //Leap.z = 0.275f;
+                    Screen.x = _transformationInverse[0, 0] * Leap.x + _transformationInverse[1, 0] * Leap.y + _transformationInverse[2, 0] * Leap.z + _transformationInverse[3, 0];
+                    Screen.y = _transformationInverse[0, 1] * Leap.x + _transformationInverse[1, 1] * Leap.y + _transformationInverse[2, 1] * Leap.z + _transformationInverse[3, 1];
+                    Screen.z = _transformationInverse[0, 2] * Leap.x + _transformationInverse[1, 2] * Leap.y + _transformationInverse[2, 2] * Leap.z + _transformationInverse[3, 2];
+                    if (hand.GrabStrength >= .5)
+                    {
+                        PointPair pointPair = new PointPair(Screen.x, Screen.y);
+                        PointPairList pointPairs = new PointPairList();
+                        pointPairs.Add(pointPair);
+                        LineItem line = graphPane.AddCurve("", pointPairs, Color.Black);
+                    }
+                    zedGraphControl1.Invalidate();
+                    ScreenPosX.Text = Screen.x.ToString();
+                    ScreenPosY.Text = Screen.y.ToString();
+                    ScreenPosZ.Text = Screen.z.ToString();
+                }
+            }
+            else
+            {
+                handPosX.Text = "NA";
+                handPosY.Text = "NA";
+                handPosZ.Text = "NA";
+            }
+        }
+
+        private void Controller_Device(object sender, DeviceEventArgs e)
+        {
+            
+        }
+
+        private void Controller_Connect(object sender, ConnectionEventArgs e)
+        {
+            
+        }
+
+        private void cmdDisconnectLeapMotion_Click(object sender, EventArgs e)
+        {
+            if (controller.IsConnected)
+                controller.StopConnection();
+        }
+
+        private void cmdSetOrigin_Click(object sender, EventArgs e)
+        {
+            _OriginPoint.x = Leap.x;
+            _OriginPoint.y = Leap.y;
+            _OriginPoint.z = Leap.z;
+
+            //testing-test
+            //_OriginPoint.x = 0;
+            //_OriginPoint.y = 0;
+            //_OriginPoint.z = 0.275F;
+            OriginX.Text = _OriginPoint.x.ToString();
+            OriginY.Text = _OriginPoint.y.ToString();
+            OriginZ.Text = _OriginPoint.z.ToString();
+        }
+
+        private void cmdSetAxis1_Click(object sender, EventArgs e)
+        {
+            float _Axis1X = Leap.x;
+            float _Axis1Y = Leap.y;
+            float _Axis1Z = Leap.z;
+
+            //testing
+            //float _Axis1X = 1;
+            //float _Axis1Y = 0;
+            //float _Axis1Z = 0.275f;
+            AxisX = new Vector(_Axis1X - _OriginPoint.x, _Axis1Y - _OriginPoint.y, _Axis1Z - _OriginPoint.z);
+            PAxisX.Text = AxisX.x.ToString();
+            PAxisY.Text = AxisX.y.ToString();
+            PAxisZ.Text = AxisX.z.ToString();
+        }
+
+        private void cmdSetAxis2_Click(object sender, EventArgs e)
+        {
+            float _Axis2X = Leap.x;
+            float _Axis2Y = Leap.y;
+            float _Axis2Z = Leap.z;
+            //testing
+            //float _Axis2X = 1;
+            //float _Axis2Y = 1;
+            //float _Axis2Z = 0.275f;
+            Vector Axis2 = new Vector(_Axis2X - _OriginPoint.x, _Axis2Y - _OriginPoint.y, _Axis2Z - _OriginPoint.z);
+
+            //changes
+            AxisZ = AxisX.Cross(Axis2);
+            //AxisZ = new Vector(AxisX.y * Axis2.z - AxisX.z * Axis2.y,
+            //                        AxisX.z * Axis2.x - AxisX.x * Axis2.z,
+            //                        AxisX.x * Axis2.y - AxisX.y * Axis2.x);
+            Vector UnitAxisZ = AxisZ.Normalized;
+            Vector UnitAxisX = AxisX.Normalized;
+            
+            //changes
+            Vector UnitAxisY = -UnitAxisX.Cross(UnitAxisZ);
+            //Vector UnitAxisY = new Vector(-(UnitAxisX.y * UnitAxisZ.z - UnitAxisX.z * UnitAxisZ.y),
+            //                              -(UnitAxisX.z * UnitAxisZ.x - UnitAxisX.x * UnitAxisZ.z),
+            //                              -(UnitAxisX.x * UnitAxisZ.y - UnitAxisX.y * UnitAxisZ.x));
+            QAxisX.Text = UnitAxisY.x.ToString();
+            QAxisY.Text = UnitAxisY.y.ToString();
+            QAxisZ.Text = UnitAxisY.z.ToString();
+
+            _orientationScreen[0, 0] = UnitAxisX.x;
+            _orientationScreen[1, 0] = UnitAxisY.x;
+            _orientationScreen[2, 0] = UnitAxisZ.x;
+
+            _orientationScreen[0, 1] = UnitAxisX.y;
+            _orientationScreen[1, 1] = UnitAxisY.y;
+            _orientationScreen[2, 1] = UnitAxisZ.y;
+
+            _orientationScreen[0, 2] = UnitAxisX.z;
+            _orientationScreen[1, 2] = UnitAxisY.z;
+            _orientationScreen[2, 2] = UnitAxisZ.z;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    _orientationScreenTransposed[i, j] = _orientationScreen[j, i];
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    _transformationInverse[i, j] = _orientationScreenTransposed[i, j];
+                }
+            }
+            _transformationInverse[3, 0] = -(_orientationScreenTransposed[0, 0] * _OriginPoint.x + _orientationScreenTransposed[1, 0] * _OriginPoint.y + _orientationScreenTransposed[2, 0] * _OriginPoint.z);
+            _transformationInverse[3, 1] = -(_orientationScreenTransposed[0, 1] * _OriginPoint.x + _orientationScreenTransposed[1, 1] * _OriginPoint.y + _orientationScreenTransposed[2, 1] * _OriginPoint.z);
+            _transformationInverse[3, 2] = -(_orientationScreenTransposed[0, 2] * _OriginPoint.x + _orientationScreenTransposed[1, 2] * _OriginPoint.y + _orientationScreenTransposed[2, 2] * _OriginPoint.z);
+            _transformationInverse[0, 3] = _transformationInverse[1, 3] = _transformationInverse[2, 3] = 0;
+            _transformationInverse[3, 3] = 1;
+
+            isTransformed = true;
+        }
+
+        private void CmdSetPoint2_Click(object sender, EventArgs e)
+        {
+            Point2.x = Leap.x;
+            Point2.y = Leap.y;
+            Point2.z = Leap.z;
+            distance.Text = Point1.DistanceTo(Point2).ToString();
+        }
+
+        private void CmdSetPoint1_Click(object sender, EventArgs e)
+        {
+            Point1.x = Leap.x;
+            Point1.y = Leap.y;
+            Point1.z = Leap.z;
+        }
+    }
+}
